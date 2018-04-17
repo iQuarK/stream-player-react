9c350d98ff53c1012968880e9390112524c9fbcd
Luis Sanchez Castellanos <luis.sanchez@schibsted.com>
2018-04-17 23:28:00 +0100

    Basic Player layout

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
 import "./Player.css";
 
 class Player extends Component {
+    static propTypes = {
+        song: PropTypes.shape({
+            title: PropTypes.string
+        })
+    };
+
+    state = {
+        playing: false
+    };
+
+    togglePlay = () => this.setState({ playing: !this.state.playing });
+
     render() {
-        return <div className="player">Here should be a player!</div>;
+        const { playing } = this.state;
+
+        return (
+            <div className="player">
+                <div className="play">
+                    <i
+                        className={classnames("audio main-button", {
+                            ["play-button"]: !playing,
+                            ["pause-button"]: playing
+                        })}
+                        onClick={this.togglePlay}
+                    />
+                    <div className="g-col">
+                        <span className="playing-title">Playing:</span>
+                        <div className="song-title">title!</div>
+                    </div>
+                </div>
+                <div className="progress-bar">
+                    <div className="bar" />
+                </div>
+            </div>
+        );
     }
 }
 
