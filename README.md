6f9c12c71e3ad2a5bc4cbc329f6c06288b5e785b
Luis Sanchez Castellanos <luis.sanchez@schibsted.com>
2018-04-18 16:17:28 +0100

    Loading songs from S3

--- README.md
+++ README.md
@@ -4,8 +4,22 @@
 
 1. ~~Stream server accessing local files + basic client.~~
 2. ~~Retrieve playlist from server using Redux~~
-2. Accessing Remote host.
+2. ~~Accessing Remote host.~~
 3. Broadcasting what are you listening.
 4. Stream server seeking positions.
 5. Radio mode.
 
+## CONFIGURATION
+The configuration file `config.json` can be found in the root directory, there you can decide to load the songs
+from `local` or `S3`.
+
+### Local
+If you load from local, the songs must be in the `audios` folder.
+
+### S3
+To load songs from an S3 bucket you must set the following environment variables:
+```
+ $ export AWS_ACCESS_KEY_ID="Your AWS access key"
+ $ export AWS_SECRET_ACCESS_KEY="Your AWS secret access key"
+ $ export STREAM_BUCKET="your-bucket-name"
+```
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const app = express();
 const http = require("http").Server(app);
 const fs = require("fs");
 const config = require("./config.json");
+const AWS = require("aws-sdk");
+const BUCKET = process.env.STREAM_BUCKET;
 
 app.use(cors());
 app.use(express.static("build"));
@@ -19,6 +21,7 @@ app.get("/listen/:filename", (req, res) => {
     let myReadStream;
 
     res.writeHead(200, { "Content-Type": "audio/mpeg" });
+
     if (config.songs.locationType === "local") {
         const path = __dirname + `/audios/${filename}`;
         const endBytes = fs.statSync(path).size;
@@ -27,6 +30,10 @@ app.get("/listen/:filename", (req, res) => {
             end: endBytes - 1,
             autoClose: true
         });
+    } else if (config.songs.locationType === "S3") {
+        const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+        const params = { Bucket: BUCKET, Key: filename };
+        myReadStream = s3.getObject(params).createReadStream();
     }
 
     myReadStream.pipe(res);
